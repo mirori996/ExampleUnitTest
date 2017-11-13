ff2f295163c7de6bd873377f266c4ac76d1e841b
Tamahashi Yutaro <e175743@ie.u-ryukyu.ac.jp>
2017-11-13 19:35:25 +0900

    HeroとEnemyの共通部分をLivingThingにまとめて、LivingThingをHeroとEnemyに継承させた。テストが失敗したので、attack()も書き換えた。

--- src/main/jp/ac/uryukyu/ie/e175743/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175743/Enemy.java
@@ -8,49 +8,16 @@ package jp.ac.uryukyu.ie.e175743;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
-
+public class Enemy extends LivingThing {
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
+     *
+     * @param name      ヒーロー名
+     * @param maximumHP ヒーローのHP
+     * @param attack    ヒーローの攻撃力
      */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        if(hitPoint > 0) {
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
+    public Enemy(String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -58,6 +25,7 @@ public class Enemy {
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
